4adb2fc382b5c6869b95374c5ffd77af5d8397cc
Tony Pereira <tony@mbp-de-tony.lan>
2020-04-29 17:08:44 +0200

    Finalisation des exercices

---
+++ Bash/compt.sh
@@ -0,0 +1,7 @@
+cpt=0
+while ((cpt<=5))
+    do
+        echo "$cpt"
+        ((cpt+=1))
+    done
+exit 0
---
+++ Bash/nameUser.sh
@@ -0,0 +1,4 @@
+echo Salut comment tu t\'appelles ?
+read name
+DATE=$(date +%d/%m/%Y)
+echo Enchantee $name, nous sommes le ${DATE}
